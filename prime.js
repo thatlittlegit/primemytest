0bbb96fc02f773dac58d943753216bdf9464591a
wapidstyle <wapidstyle@live.ca>
2017-02-19 07:43:38 -0500

    Rewrote most of it
    It needed it. MAY BE INACCURATE.
    
    Note: -e is not implemented.

--- prime.js
+++ prime.js
@@ -1,46 +1,54 @@
-/*
-  PrimeMyTest 1.0.0
- */
-function prime() {
-  "use strict";
-  const jQuery = require("jquery");
-  const $ = jQuery;
-  const util = require("util");
-  let current = 0;
-  let run = true;
+const argv = require("minimist")(process.argv.slice(2));
+const _ = {
+  find: require("lodash.find"),
+  isEmpty: require("lodash.isEmpty")
+};
 
-  const rl = require("readline").createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-  rl.on("line", function(){
-    run = false;
-  });
 
-  function test() {
-    current++;
-    let work = true;
-    for (let i = 2; i < current; i++) {
-      if (current % i === 0 && i !== 1) {
-        work = false;
-        break;
-      }
-    }
-    if (work === false) {
-      return false;
-    } else {
-      return true;
+let benchmarking = false;
+let toFile = false;
+let help = false;
+
+if(!(_.isEmpty(argv._)) || argv.benchmarking || argv.toFile || argv.help ||
+    argv.b || argv.f || argv.h) {
+  benchmarking = argv.b || argv.benchmarking || false;
+  toFile = argv.f || argv.toFile || false;
+  help = argv.h || argv.help || false;
+}
+
+if(help) {
+  console.log("node prime.js [-bfh]");
+  console.log("              -h");
+  console.log("          --help  Display this help message");
+  console.log("");
+  console.log("              -b");
+  console.log("  --benchmarking  Enable benchmarking mode, to test time until 100K calulated");
+  console.log("");
+  console.log("              -f");
+  console.log("          --file  Write to a file (prime.txt)");
+  console.log("");
+  console.log("              -e");
+  console.log("          --even  Only do odd numbers, except for 2");
+  process.exit(0);
+}
+
+let currentValue = 1;
+let max = 100001;
+let start = Date.now();
+let bar = require("another-progress-bar")("prime", 60);
+for(; currentValue < max; currentValue += 1) {
+  let isPrime = true;
+  for(let currentModValue = 2; currentModValue < currentValue + 1; currentModValue += 1) {
+    if(currentValue % currentModValue === 0 && !(currentModValue === 1 || currentModValue === currentValue)) {
+      isPrime = false;
     }
   }
-  let out;
-  while (run === true) {
-    out = test();
-    require("fs").appendFile("prime.txt", current + ": " + out, "utf-8", function(e) {
-      if (e) {
-        throw e;
-      }
-    });
-    console.log(current + ": " + out);
+  if(!benchmarking) {
+    console.log(currentValue + ": " + isPrime);
+  } else {
+    bar.progress(currentValue, max);
   }
 }
-prime();
+if(benchmarking) {
+  console.log("Your score is: " + (Date.now() - start));
+}
