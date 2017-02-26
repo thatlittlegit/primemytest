1e5c00c90c6c270123006392096819d66a189b9b
wapidstyle <wapidstyle@live.ca>
2017-02-26 09:01:11 -0500

    Add even capabilities.

--- prime.js
+++ prime.js
@@ -4,20 +4,22 @@ const _ = {
   isEmpty: require("lodash.isEmpty")
 };
 
-
 let benchmarking = false;
 let toFile = false;
 let help = false;
+let even = false;
 
-if(!(_.isEmpty(argv._)) || argv.benchmarking || argv.toFile || argv.help ||
-    argv.b || argv.f || argv.h) {
+if (!(_.isEmpty(argv._)) ||
+    argv.benchmarking || argv.toFile || argv.help || argv.even ||
+    argv.b || argv.f || argv.h || argv.e) {
   benchmarking = argv.b || argv.benchmarking || false;
   toFile = argv.f || argv.toFile || false;
   help = argv.h || argv.help || false;
+  even = argv.e || argv.even || false;
 }
 
-if(help) {
-  console.log("node prime.js [-bfh]");
+if (help) {
+  console.log("node prime.js [-bfhe]");
   console.log("              -h");
   console.log("          --help  Display this help message");
   console.log("");
@@ -36,19 +38,30 @@ let currentValue = 1;
 let max = 100001;
 let start = Date.now();
 let bar = require("another-progress-bar")("prime", 60);
-for(; currentValue < max; currentValue += 1) {
+let doEvens = true;
+
+for (; currentValue < max; currentValue += 1) {
   let isPrime = true;
-  for(let currentModValue = 2; currentModValue < currentValue + 1; currentModValue += 1) {
-    if(currentValue % currentModValue === 0 && !(currentModValue === 1 || currentModValue === currentValue)) {
+  if (currentValue % 2 === 0) {
+    currentValue++;
+  }
+  for (let currentModValue = 2; currentModValue < currentValue + 1; currentModValue += 1) {
+    if (currentValue % currentModValue === 0 && !(currentModValue === 1 || currentModValue === currentValue)) {
       isPrime = false;
     }
   }
-  if(!benchmarking) {
+
+  // TODO Make this follow Grunt's styling better
+  if (!benchmarking) {
     console.log(currentValue + ": " + isPrime);
   } else {
     bar.progress(currentValue, max);
   }
+
+  if (currentValue === 2 && even) {
+    doEvens = true;
+  }
 }
-if(benchmarking) {
+if (benchmarking) {
   console.log("Your score is: " + (Date.now() - start));
 }
