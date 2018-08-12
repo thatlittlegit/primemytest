52a96c8bb1a62018c26295bef9fd1dee1cc5b601
thatlittlegit <personal@thatlittlegit.tk>
2018-08-12 13:35:06 +0200

    BREAKING: use subcommands not flags

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const argv = require("commander");
-
-argv
-    .version(JSON.parse(require('fs').readFileSync('package.json', 'utf-8')).version)
-    .option('-b, --benchmarking', 'enable benchmarking mode, to test time until 100K calculated')
-    .option('-i, --interactive', 'enable interactive mode; no effect unless used with --benchmarking')
-    .option('-e, --even', 'only do odd numbers')
-    .parse(process.argv);
-
 const findAllPrimesUntilNumberAndCallCallbackForEach = (number, limit, cb, dontDoEvens) => {
   while (number++ < limit) {
     if (dontDoEvens ? number % 2 !== 0 : true) cb(number, isPrime(number));
@@ -24,18 +15,36 @@ const getNumberOfFactors = number => (
     .filter(value => value)
     .length
 );
-let start = Date.now();
-let doEvens = true;
 
-if (argv.benchmarking) {
-  if (argv.interactive) console.log("\x1B[33mWARNING: interactive mode can be slower than non-interactive!\x1B[m");
-  let start = Date.now();
-  findAllPrimesUntilNumberAndCallCallbackForEach(1, 10000, argv.interactive ? number => {
-    process.stdout.write(`\x1B[2K\x1B[1G${String(number).padStart(6, '0')} of 100000 (${String(Date.now() - start).padStart(8, '0')})`);
-  } : () => {});
+const program = require("commander");
 
-  if (argv.interactive) console.log("");
-  console.log(Date.now() - start);
-} else {
-    findAllPrimesUntilNumberAndCallCallbackForEach(1, Infinity, (number, result) => console.log(`${number}\t${result}`), argv.even);
-}
+program
+    .version(JSON.parse(require('fs').readFileSync('package.json', 'utf-8')).version)
+    .option('-e, --even', 'only do odd numbers');
+
+program
+    .command('benchmarking')
+    .alias('b')
+    .description('enable benchmarking mode; to test time until 100K calculated')
+    .option('-i, --interactive', 'enable interactive mode')
+    .action(options => {
+        const interactive = options.interactive;
+        if (interactive) console.log("\x1B[33mWARNING: interactive mode can be slower than non-interactive!\x1B[m");
+        let start = Date.now();
+        findAllPrimesUntilNumberAndCallCallbackForEach(1, 10000, interactive ? number => {
+            process.stdout.write(`\x1B[2K\x1B[1G${String(number).padStart(6, '0')} of 100000 (${String(Date.now() - start).padStart(8, '0')})`);
+        } : () => {}, Boolean(program.even));
+
+        if (interactive) console.log("");
+        console.log(Date.now() - start);
+    })
+
+program
+    .command('calculate', {isDefault: true})
+    .alias('c')
+    .description('calculate prime numbers and output to STDOUT')
+    .action(() => {
+        findAllPrimesUntilNumberAndCallCallbackForEach(1, Infinity, (number, result) => console.log(`${number}\t${result}`), Boolean(program.even));
+    });
+
+program.parse(process.argv);
