99da5adf7861e3a0ff0a31c13103d3b812fc7db3
thatlittlegit <personal@thatlittlegit.tk>
2018-08-12 13:13:52 +0200

    Migrate from Minimist to Commander

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-const argv = require("minimist")(process.argv.slice(2));
+const argv = require("commander");
 
-let benchmarking = argv.b || argv.benchmarking || false;
-let help = argv.h || argv.help || false;
-let even = argv.e || argv.even || false;
-let interactive = argv.i || argv.interactive || false;
-
-if (help) {
-  console.log("node prime.js [-bfhe]");
-  console.log("              -h");
-  console.log("          --help  Display this help message");
-  console.log("");
-  console.log("              -b");
-  console.log("  --benchmarking  Enable benchmarking mode, to test time until 100K calulated");
-  console.log("");
-  console.log("              -i");
-  console.log("   --interactive  Enable interactive mode. No effect unless used with --benchmarking.");
-  console.log("");
-  console.log("              -e");
-  console.log("          --even  Only do odd numbers, except for 2");
-  process.exit(0);
-}
+argv
+    .version(JSON.parse(require('fs').readFileSync('package.json', 'utf-8')).version)
+    .option('-b, --benchmarking', 'enable benchmarking mode, to test time until 100K calculated')
+    .option('-i, --interactive', 'enable interactive mode; no effect unless used with --benchmarking')
+    .option('-e, --even', 'only do odd numbers')
+    .parse(process.argv);
 
 const findAllPrimesUntilNumberAndCallCallbackForEach = (number, limit, cb, dontDoEvens) => {
   while (number++ < limit) {
@@ -41,15 +27,15 @@ const getNumberOfFactors = number => (
 let start = Date.now();
 let doEvens = true;
 
-if (benchmarking) {
-  if (interactive) console.log("\x1B[33mWARNING: interactive mode can be slower than non-interactive!\x1B[m");
+if (argv.benchmarking) {
+  if (argv.interactive) console.log("\x1B[33mWARNING: interactive mode can be slower than non-interactive!\x1B[m");
   let start = Date.now();
-  findAllPrimesUntilNumberAndCallCallbackForEach(1, 10000, interactive ? number => {
+  findAllPrimesUntilNumberAndCallCallbackForEach(1, 10000, argv.interactive ? number => {
     process.stdout.write(`\x1B[2K\x1B[1G${String(number).padStart(6, '0')} of 100000 (${String(Date.now() - start).padStart(8, '0')})`);
   } : () => {});
 
-  if (interactive) console.log("");
+  if (argv.interactive) console.log("");
   console.log(Date.now() - start);
 } else {
-    findAllPrimesUntilNumberAndCallCallbackForEach(1, Infinity, (number, result) => console.log(`${number}\t${result}`), even);
+    findAllPrimesUntilNumberAndCallCallbackForEach(1, Infinity, (number, result) => console.log(`${number}\t${result}`), argv.even);
 }
