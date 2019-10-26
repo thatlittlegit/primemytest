36202be0e2d817cc1db9d14b2e33cf7561e8acfd
thatlittlegit <personal@thatlittlegit.tk>
2019-10-25 22:06:03 -0400

    c: remove syscall-only mode
    It was only slower compared to the indirect variant.

--- c/prime.c
+++ c/prime.c
@@ -3,22 +3,9 @@
 #include <string.h>
 #include <math.h>
 
-#ifdef USE_SYSCALLS
-#include <stdlib.h>
-#include <unistd.h>
-
-// minimum size 50
-char* globalBuffer = NULL;
-
-static inline void printstatus(int num, bool isPrime) {
-	snprintf(globalBuffer, 50, "%d\t%s\n", num, isPrime ? "true" : "false");
-	register int ignore = write(STDOUT_FILENO, globalBuffer, strlen(globalBuffer));
-}
-#else
 static inline void printstatus(int num, bool isPrime) {
 	printf("%d\t%s\n", num, isPrime ? "true" : "false");
 }
-#endif
 
 static inline bool calculateprime(int num) {
 	double provided = sqrt((double) num);
@@ -33,10 +20,6 @@ static inline bool calculateprime(int num) {
 }
 
 int main() {
-#ifdef USE_SYSCALLS
-	globalBuffer = malloc(50);
-#endif
-
 	for(register int i = 2; i <= 100000; i++) {
 		printstatus(i, calculateprime(i));
 	}
